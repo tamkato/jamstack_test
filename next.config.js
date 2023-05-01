c14312d1d70d3f824ad39314df3598d54bb33e67
加藤晟也 <kato.s@tam-tam.co.jp>
2023-05-01 18:53:00 +0900

    23th コミット

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   trailingSlash: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+}
 
-// // module.exports = nextConfig
+module.exports = nextConfig
 
 // module.exports = {
 //   distDir: 'out',
 // }
 
-module.exports = {
-  distDir: 'out',
-  output: {
-    // この設定により、静的ファイルが生成された際に、トレーリングスラッシュが追加されます
-    exportTrailingSlash: true,
-  },
-  // この関数により、各ページに対応するパスを定義します
-  async exportPathMap() {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-    };
-  },
-};
+// module.exports = {
+//   distDir: 'out',
+//   output: {
+//     // この設定により、静的ファイルが生成された際に、トレーリングスラッシュが追加されます
+//     exportTrailingSlash: true,
+//   },
+//   // この関数により、各ページに対応するパスを定義します
+//   async exportPathMap() {
+//     return {
+//       '/': { page: '/' },
+//       '/about': { page: '/about' },
+//     };
+//   },
+// };
