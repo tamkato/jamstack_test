f94ef96dfac3713b05d8445e6b40e448278f7188
加藤晟也 <kato.s@tam-tam.co.jp>
2023-05-02 14:36:02 +0900

    change index page

--- next.config.js
+++ next.config.js
@@ -5,22 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// module.exports = {
-//   distDir: 'out',
-// }
-
-// module.exports = {
-//   distDir: 'out',
-//   output: {
-//     // この設定により、静的ファイルが生成された際に、トレーリングスラッシュが追加されます
-//     exportTrailingSlash: true,
-//   },
-//   // この関数により、各ページに対応するパスを定義します
-//   async exportPathMap() {
-//     return {
-//       '/': { page: '/' },
-//       '/about': { page: '/about' },
-//     };
-//   },
-// };
